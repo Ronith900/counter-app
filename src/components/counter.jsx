b984e85d70a4e34ae01ff2b950484838ea4528c4
Ronith <44728044+Ronith900@users.noreply.github.com>
2021-06-07 12:27:19 +0800

    enh

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -38,7 +38,7 @@ class Counter extends Component {
     return (
       <div>
 
-          <h4>Counter {this.props.counter.id}</h4>
+          <h4>Counters app {this.props.counter.id}</h4>
         <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary" onClick={() => this.props.onIncrement(this.props.counter)}>Increment</button>
         <button className="btn btn-danger m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
